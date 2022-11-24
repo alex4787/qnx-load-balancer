ce8f6338231c782538bd8115b9d4ca3c2afeb53b
Alex Chan <alex.chan@shopify.com>
2022-11-24 13:33:51 -0500

    Refactored

--- main2.c
+++ main2.c
@@ -12,7 +12,6 @@
 #include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/debug.h>
 #include <sys/procfs.h>
@@ -24,6 +23,9 @@
 #include <devctl.h>
 #include <errno.h>
 #include <dirent.h>
+#include <sys/trace.h>
+#include <sys/kercalls.h>
+#include <sys/procmgr.h>
 
 #define TRUE 0
 #define FALSE 1
@@ -36,261 +38,283 @@
 #define LOAD_PROPORTION_THRESHOLD 0.2
 
 struct load_state_t {
-	unsigned int task_ready_count;
-	unsigned int task_sleep_count;
-	debug_thread_t current_task;
-	debug_thread_t min_sleep_task;
-	debug_thread_t min_task;
-	unsigned long int totaltime;
+	debug_thread_t *min_task;
+	uint64_t totaltime;
 } load_state_t;
 
-// might not need num_tasks
-// if don't need num tasks we don't need a struct holding 1 thing --> we could just use int
-// could use total time from load_state_t struct ^
-struct cpu_loads_t {
-	debug_thread_t *threads;
-	int total_time;
-	int num_tasks;
-} cpu_loads_t;
-
 static struct load_state_t LoadStates[MAX_CPUS];
-static struct cpu_loads_t CpuLoads[MAX_CPUS];
 
 static int NumCpus = 0;
-static int min_cpu, max_cpu, min_load, max_load;
-static float ave_load;
+static int MinCpu, MaxCpu, MinLoad, MaxLoad;
+static float AveLoad;
 
-void find_min_task(int cpu, debug_thread_t thread, struct load_state_t loads[], int *first_task) {
-	if (first_task == TRUE) {
-		loads[cpu].min_task = thread;
-		*first_task = FALSE;
-	} else if (thread.state == STATE_RUNNING || (thread.state != STATE_STOPPED && thread.state != STATE_DEAD)) {
-		if (thread.sutime < loads[cpu].min_task.sutime) {
-			loads[cpu].min_task = thread;
-		}
+void populate_load_state(int cpu, debug_thread_t thread) {
+	struct load_state_t *cur_load_state = LoadStates + cpu;
+
+	if (cur_load_state->min_task == NULL) {
+		cur_load_state->min_task = malloc(sizeof(load_state_t));
+		memcpy(cur_load_state->min_task, &thread, sizeof(load_state_t));
+		printf("new min_task for cpu=%d: pid=%d tid=%d\n", cpu, thread.pid,
+				thread.tid);
+	} else if (thread.sutime < cur_load_state->min_task->sutime) {
+		memcpy(cur_load_state->min_task, &thread, sizeof(load_state_t));
+		printf("new min_task for cpu=%d: pid=%d tid=%d\n", cpu, thread.pid,
+				thread.tid);
 	}
-}
-void populate_loads_struct(int cpu, debug_thread_t thread, struct load_state_t loads[], int *first_sleep_task) {
-	if (thread.state == STATE_RUNNING) {
-		loads[cpu].current_task = thread;
 
-		printf("found running task\n");
-	} else if (thread.state == STATE_READY) {
-		loads[cpu].task_ready_count += 1;
-		loads[cpu].totaltime += thread.sutime;
-
-		printf("total time: %ld\n", loads[cpu].totaltime);
-	} else if (thread.state != STATE_STOPPED && thread.state != STATE_DEAD) {
-		loads[cpu].task_sleep_count += 1;
-		if (first_sleep_task == TRUE) {
-			LoadStates[cpu].min_sleep_task = thread;
-			*first_sleep_task = FALSE;
-			printf("first min sleep task set\n");
-		} else if (thread.sutime < loads[cpu].min_sleep_task.sutime) {
-			loads[cpu].min_sleep_task = thread;
-			printf("new min sleep task: %d\n", loads[cpu].min_sleep_task.sutime);
-		}
-	}
-}
-
-void sampleCpus(struct cpu_loads_t cpus[], int cpu, debug_thread_t thread) {
 	if (thread.state == STATE_READY) {
-		cpus[cpu].total_time += thread.sutime;
-		cpus[cpu].num_tasks += 1;
+		cur_load_state->totaltime += thread.sutime;
+
+		printf("total time: %ld\n", cur_load_state->totaltime);
 	}
 }
 
-void find_min_max_cpus(struct cpu_loads_t cpuLoads[]) {
+void calculate_cpu_loads() {
 	float sum;
-	min_load = 0;
-	max_load = 0;
-	min_cpu = 0;
-	max_cpu = 0;
 
-	for (int i=0; i<NumCpus; i++) {
-		sum += cpuLoads[i].total_time;
+	MinLoad = -1;
+	MaxLoad = -1;
+	MinCpu = -1;
+	MaxCpu = -1;
+	AveLoad = -1;
+
+	for (int i = 0; i < NumCpus; i++) {
+		uint64_t totaltime = LoadStates[i].totaltime;
+
+		sum += totaltime;
+
+		if (MinLoad == -1 || totaltime < MinLoad) {
+			MinLoad = totaltime;
+			MinCpu = i;
+		}
+		if (MaxLoad == -1 || totaltime > MaxLoad) {
+			MaxLoad = totaltime;
+			MaxCpu = i;
+		}
 	}
 
-	ave_load = sum / NumCpus;
-
-	for (int i=0; i<NumCpus; i++) {
-		if (cpuLoads[i].total_time < min_load) {
-			min_load = cpuLoads[i].total_time;
-			min_cpu = i;
-		}
-		if (cpuLoads[i].total_time > max_load) {
-			max_load = cpuLoads[i].total_time;
-			max_cpu = i;
-		}
-	}
+	AveLoad = sum / NumCpus;
 }
 
 int get_load_state(int processor_load, int avg_processor_load) {
-	if (processor_load > avg_processor_load * (1-LOAD_PROPORTION_THRESHOLD)) {
+	if (processor_load > avg_processor_load * (1 - LOAD_PROPORTION_THRESHOLD)) {
 		return HEAVY_LOAD;
-	} else if (avg_processor_load * (1+LOAD_PROPORTION_THRESHOLD) >= processor_load && processor_load >= avg_processor_load * (1-LOAD_PROPORTION_THRESHOLD)) {
+	} else if (avg_processor_load * (1 + LOAD_PROPORTION_THRESHOLD)
+			>= processor_load
+			&& processor_load
+					>= avg_processor_load * (1 - LOAD_PROPORTION_THRESHOLD)) {
 		return NORMAL_LOAD;
-	} else if (processor_load < avg_processor_load * (1-LOAD_PROPORTION_THRESHOLD)) {
+	} else if (processor_load
+			< avg_processor_load * (1 - LOAD_PROPORTION_THRESHOLD)) {
 		return LIGHT_LOAD;
 	} else {
 		return -1; // something went wrong
 	}
 }
 
-int partA() {
-	DIR			*dir;
-	char		fname[PATH_MAX];
-	char		slay_call[256];
+int run_load_balancer() {
+	DIR *dir;
+	char fname[PATH_MAX];
 
 	dir = opendir("/proc");
 
 	if (dir == NULL) {
 		fprintf(stderr, "Unable to open %s\n", fname);
-		return 0;
-	} else {
-		struct dirent * dirent;
-		/* Reset values */
-		debug_process_t procinfo;
-		for (int i=0; i<NumCpus; i++) {
-			LoadStates[i].task_ready_count = 0;
-			LoadStates[i].task_sleep_count = 0;
-			LoadStates[i].totaltime = 0;
+		return -1;
+	}
 
-			CpuLoads[i].total_time = 0;
-			CpuLoads[i].num_tasks = 0;
-		}
-		int first_sleep_task = TRUE;
-		int first_task = TRUE;
+	struct dirent *dirent;
+	debug_process_t procinfo;
 
-		while ((dirent = readdir(dir)) != NULL) {
-			memset(&procinfo, 0, sizeof(procinfo));
-			if (isdigit(dirent->d_name[0])) {
-				int pid = atoi(dirent->d_name);
-				int fd;
-				char buff[512];
-				int procstatus;
+	// Reset values
+	for (int i = 0; i < NumCpus; i++) {
+		LoadStates[i].min_task = NULL;
+		LoadStates[i].totaltime = 0;
+	}
 
+	// Loop through all processes (all subdirectories of /proc)
+	while ((dirent = readdir(dir)) != NULL) {
+		memset(&procinfo, 0, sizeof(procinfo));
+		if (isdigit(dirent->d_name[0])) {
+			int pid = atoi(dirent->d_name);
 
-				snprintf(buff, sizeof(buff), "/proc/%d/as", pid);
-				printf("pid:%d\n", pid);
+			// Skip procnto
+			if (pid == 1) {
+				continue;
+			}
 
-				if ((fd = open(buff, O_RDONLY)) == -1) {
-					return 0;
-				}
+			int fd;
+			char buff[512];
+			int procstatus;
 
-				if ((procstatus = devctl(fd, DCMD_PROC_INFO, &procinfo, sizeof procinfo, 0)) != -1) {
-					int lasttid, tid, cpu;
-					printf("proc_status=%d\n", procstatus);
-					printf("num_threads=%d\n", procinfo.num_threads);
-					if (procinfo.flags & _NTO_PF_ZOMBIE) {
-						close(fd);
-						continue;
-					} else {
-						for (lasttid = tid = 1; ; lasttid = ++tid) {
-							int status;
-							debug_thread_t threadinfo;
-							memset(&threadinfo, 0, sizeof(threadinfo));
-							threadinfo.tid = tid;
-							if ((status = devctl(fd, DCMD_PROC_TIDSTATUS, &threadinfo, sizeof(threadinfo), 0)) != EOK) {
-								printf("status=%d\n", status);
-								break;
-							}
-							tid = threadinfo.tid;
-							if (tid < lasttid) {
-								break;
-							}
+			snprintf(buff, sizeof(buff), "/proc/%d/as", pid);
 
-							// Populate load_state_t array
-							cpu = threadinfo.last_cpu;
-							printf("CPU: %d\n", cpu);
+			if ((fd = open(buff, O_RDONLY)) == -1) {
+				return -1;
+			}
 
+			// Get process info
+			if ((procstatus = devctl(fd, DCMD_PROC_INFO, &procinfo,
+					sizeof procinfo, 0)) != -1) {
+				int lasttid, tid, cpu;
 
-							// Part A
-							populate_loads_struct(cpu, threadinfo, LoadStates, &first_sleep_task);
-							find_min_task(cpu, threadinfo, LoadStates, &first_task);
+				printf("\npid=%d [proc_status=%d num_threads=%d]\n", pid,
+						procstatus, procinfo.num_threads);
 
-							// Part B
-							// could refactor to use total_time from LoadStates instead of sampleCpus
-
-							sampleCpus(CpuLoads, cpu, threadinfo);
-							find_min_max_cpus(CpuLoads);
-							int load_state_min_core = get_load_state(min_load, ave_load);
-							int load_state_max_core = get_load_state(max_load, ave_load);
-
-							if (load_state_max_core != HEAVY_LOAD || load_state_max_core != LIGHT_LOAD) {
-								printf("no migration needed 1\n");
-								// no migration needed
-								break; // ??
-							}
-
-
-							// Part B part 5 to check if migration needed
-							if (LoadStates[min_cpu].min_task.sutime + min_load >= max_load) {
-								printf("no migration needed 2\n");
-								// no migration
-								break;
-							} else {
-								sprintf(slay_call, "slay -m pid %d -C %d -T %d", pid, min_cpu, tid);
-								system(slay_call);
-								printf("making slay call\n");
-							}
+				if (procinfo.flags & _NTO_PF_ZOMBIE) {
+					close(fd);
+					continue;
+				} else {
+					// Loop through all threads in process
+					for (lasttid = tid = 1;; lasttid = ++tid) {
+						int status;
+						procfs_status threadinfo;
+						threadinfo.tid = tid;
+						// Get thread info
+						if ((status = devctl(fd, DCMD_PROC_TIDSTATUS,
+								&threadinfo, sizeof(threadinfo), NULL)) != EOK) {
+							printf("error status=%d\n", status);
+							break;
 						}
+						tid = threadinfo.tid;
+						if (tid < lasttid) {
+							break;
+						}
+
+						cpu = threadinfo.last_cpu;
+						printf("\ttid=%d cpu=%d\n", tid, cpu);
+
+						// Part A
+						// Populate load_state_t array
+						populate_load_state(cpu, threadinfo);
 					}
 				}
-				close(fd);
 			}
+			close(fd);
 		}
-		closedir(dir);
 	}
+	// Finished getting all thread information
+	closedir(dir);
+
+	// Part B
+	// could refactor to use total_time from LoadStates instead of sampleCpus
+
+	calculate_cpu_loads();
+	int load_state_min_core = get_load_state(MinLoad, AveLoad);
+	int load_state_max_core = get_load_state(MaxLoad, AveLoad);
+
+	// Part B4
+	if (load_state_max_core != HEAVY_LOAD || load_state_min_core != LIGHT_LOAD) {
+		printf("no migration needed 1\n");
+		return 0;
+	}
+
+	// Part B5
+	debug_thread_t min_task = *LoadStates[MaxCpu].min_task;
+	if (MinLoad + min_task.sutime >= MaxLoad - min_task.sutime) {
+		printf("no migration needed 2\n");
+		return 0;
+	}
+
+	// Migrate here
+	printf("migration needed\n");
+	int slay_pid;
+	/* sprintf(slay_call, "slay -m pid %d -C %d -T %d", pid, MinCpu, tid); */
+
+	slay_pid = spawnlp(P_NOWAIT, "slay", "slay", "-m", "pid", min_task.pid,
+			"-C", MinCpu, "-T", min_task.tid, NULL);
+	if (slay_pid == -1) {
+		printf("Unable to execute slay (%s)", strerror(errno));
+		return -1;
+	}
+
 	return 1;
 }
 
-int init_cpu( void ) {
+int init_cpu(void) {
 	int i;
 	int ProcFd = -1;
 	debug_thread_t debug_data;
 
-	memset( &debug_data, 0, sizeof( debug_data ) );
+	memset(&debug_data, 0, sizeof(debug_data));
 
 	/*
-	* Open a connection to proc to talk over.
-	*/
-	ProcFd = open( "/proc/1/as", O_RDONLY );
-	if( ProcFd == -1 ) {
+	 * Open a connection to proc to talk over.
+	 */
+	ProcFd = open("/proc/1/as", O_RDONLY);
+	if (ProcFd == -1) {
 		fprintf( stderr, "pload: Unable to access procnto: %s\n",
-		strerror( errno ) );
-		fflush( stderr );
+				strerror( errno));
+		fflush( stderr);
 		return -1;
 	}
 
-	i = fcntl(ProcFd,F_GETFD);
+	i = fcntl(ProcFd, F_GETFD);
 
-	if(i != -1) {
+	if (i != -1) {
 		i |= FD_CLOEXEC;
 
-		if(fcntl(ProcFd,F_SETFD,i) != -1) {
+		if (fcntl(ProcFd, F_SETFD, i) != -1) {
 			/* Grab this value */
 			NumCpus = _syspage_ptr->num_cpu;
+			printf("System has: %d CPUs\n", NumCpus);
 
-			partA();
-
-			return(EOK);
+			return (EOK);
 		}
-
 	}
 
 	close(ProcFd);
-	return(-1);
+	return (-1);
 }
 
+/* int do_something(event_data_t* e_d) { */
+/*	 printf("Hello\n"); */
+/*	 return 0; */
+/* } */
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
 
-	init_cpu();
-	printf("System has: %d CPUs\n", NumCpus);
+	if (init_cpu() == -1) {
+		perror("init_cpu() failed\n");
+		return -1;
+	}
 
-	while(1) {
-		sleep(1);
-		partA();
+	// Meant to add event handler for when thread created
+	/* procmgr_ability(0, */
+	/*	 PROCMGR_ADN_ROOT|PROCMGR_AOP_ALLOW|PROCMGR_AID_TRACE, */
+	/*	 PROCMGR_ADN_ROOT|PROCMGR_AOP_ALLOW|PROCMGR_AID_IO, */
+	/*	 PROCMGR_ADN_NONROOT|PROCMGR_AOP_ALLOW|PROCMGR_AID_TRACE, */
+	/*	 PROCMGR_ADN_NONROOT|PROCMGR_AOP_ALLOW|PROCMGR_AID_IO, */
+	/*	 PROCMGR_AID_EOL); */
+
+	/* TraceEvent(_NTO_TRACE_DELALLCLASSES); */
+	/* TraceEvent(_NTO_TRACE_CLRCLASSPID, _NTO_TRACE_KERCALL); */
+	/* TraceEvent(_NTO_TRACE_CLRCLASSTID, _NTO_TRACE_KERCALL); */
+	/* TraceEvent(_NTO_TRACE_CLRCLASSPID, _NTO_TRACE_THREAD); */
+	/* TraceEvent(_NTO_TRACE_CLRCLASSTID, _NTO_TRACE_THREAD); */
+
+	/* TraceEvent(_NTO_TRACE_SETALLCLASSESFAST); */
+
+	/* if (ThreadCtl(_NTO_TCTL_IO, 0)!=EOK) { */
+	/*	 (void) fprintf(stderr, "argv[0]: Failed to obtain I/O privileges\n"); */
+
+	/*	 return (-1); */
+	/* } */
+
+	/* TraceEvent(_NTO_TRACE_ADDEVENT, _NTO_TRACE_THREAD, _NTO_TRACE_THCREATE); */
+	/* TraceEvent(_NTO_TRACE_ADDEVENTHANDLER, _NTO_TRACE_THREAD, */
+	/*	 _NTO_TRACE_THCREATE, do_something, NULL); */
+	/* TraceEvent(_NTO_TRACE_START); */
+
+	/* sleep(10); */
+
+	/* TraceEvent(_NTO_TRACE_STOP); */
+	/* TraceEvent(_NTO_TRACE_FLUSHBUFFER); */
+	/* TraceEvent(_NTO_TRACE_DELEVENTHANDLER, _NTO_TRACE_THREAD, _NTO_TRACE_THCREATE); */
+
+	while (1) {
+		run_load_balancer();
+		printf("==================================================\n\n");
+		sleep(5);
 	}
 }
